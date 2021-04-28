e6e94b19a7346443d89c9d8a51a4647347a82f57
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-28 19:14:16 +0530

    Image fetch working

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const API_KEY = "QxgBRdvklgyAi2Y60vNS9knNdaiAJ9a8CHOv2L2h";
-const API_ASTRONOMY_PICTURE = "http://api.openweathermap.org/data/2.5/forecast";
+const API_ASTRONOMY_PICTURE =
+    "https://api.nasa.gov/planetary/apod?api_key=" + API_KEY + "&count=1";
 
-const astronomyImageButton = document.getElementById("astronomy-image");
 const mainContainer = document.getElementById("main-container");
 
-async function getAstronomyImageOfTheDay() {
-    const response = await fetch("API_ASTRONOMY_PICTURE");
-    const responseData = await response.json();
+getAstronomyImageOfTheDay();
 
-    console.log(responseData);
+async function getAstronomyImageOfTheDay() {
+    const response = await fetch(API_ASTRONOMY_PICTURE);
+    const data = await response.json();
+
+    console.log(data);
+
+    mainContainer.innerHTML = "";
+
+    const imageData = document.createElement("div");
+    imageData.classList.add("astronomy-image-inner");
+    imageData.innerHTML = `
+        <i class="fas fa-redo-alt refresh"></i>
+        <h2>${data[0].title}</h2>
+        <img src="${data[0].url}"></img>
+        <p>${data[0].explanation}</p>
+    `;
+
+    mainContainer.appendChild(imageData);
 }
 
-astronomyImageButton.addEventListener("click", () => {
-    getAstronomyImageOfTheDay();
-});
+//<img src="${}" alt="${}" />
