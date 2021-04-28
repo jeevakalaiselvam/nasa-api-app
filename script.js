b697b844cfeb4428251067384cb039db2366fd67
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-28 18:29:19 +0530

    Astronomy Image start

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const API_KEY = "QxgBRdvklgyAi2Y60vNS9knNdaiAJ9a8CHOv2L2h";
 const API_ASTRONOMY_PICTURE = "http://api.openweathermap.org/data/2.5/forecast";
 
-const astronomyImageContainer = document.getElementById("astronomy-image");
+const astronomyImageButton = document.getElementById("astronomy-image");
+const mainContainer = document.getElementById("main-container");
+
+async function getAstronomyImageOfTheDay() {
+    const response = await fetch("API_ASTRONOMY_PICTURE");
+    const responseData = await response.json();
+
+    console.log(responseData);
+}
+
+astronomyImageButton.addEventListener("click", () => {
+    getAstronomyImageOfTheDay();
+});
