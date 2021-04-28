7bd6c6f7a458fe68144184dbef964fdf7aadb9d5
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-28 18:05:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const API_KEY = "QxgBRdvklgyAi2Y60vNS9knNdaiAJ9a8CHOv2L2h";
+const API_ASTRONOMY_PICTURE = "http://api.openweathermap.org/data/2.5/forecast";
+
+const astronomyImageContainer = document.getElementById("astronomy-image");
